cf733cae984bd911be525a24f993ef03fb26ccdb
AhmedAly555 <ahmaly555@gmail.com>
2025-06-05 02:26:07 +0300

    #

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/splash_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      home: const SplashScreen(),
     );
   }
 }
