029f15b9563ddf282b6a348d909b07f12ee4cab5
AhmedAly555 <ahmaly555@gmail.com>
2025-06-05 17:08:16 +0300

    Add flutter_svg dependency, new SVG assets, and initial login and language widgets

---
+++ lib/auth/widgets/language_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LanguageWidget extends StatelessWidget {
+  const LanguageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Zony',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF49159B)),
       ),
       home: const SplashScreen(),
     );
