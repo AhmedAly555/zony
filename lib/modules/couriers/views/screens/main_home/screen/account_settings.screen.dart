a92548ca237b729425a6602433ff182bfef48ccd
AhmedAly555 <ahmaly555@gmail.com>
2025-07-28 22:51:34 +0300

    Refactor account settings screen: add text controllers and update layout for better structure; remove unused password text field import

--- lib/modules/couriers/views/screens/main_home/screen/account_settings.screen.dart
+++ lib/modules/couriers/views/screens/main_home/screen/account_settings.screen.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../../views/widgets/default_text_filed.dart';
 import '../../../../../../views/widgets/template_app_scaffold.widget.dart';
 import '../../../../../auth/widgets/username_text_field.widget.dart';
 
 class AccountSettingsScreen extends StatelessWidget {
-  const AccountSettingsScreen({super.key});
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+   AccountSettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return TemplateAppScaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
-              child: Row(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
+          child: Column(
+            children: [
+              Row(
                 children: [
                   Container(
                     decoration: BoxDecoration(
@@ -41,92 +46,105 @@ class AccountSettingsScreen extends StatelessWidget {
                   const Spacer(flex: 2),
                 ],
               ),
-            ),
 
-            // Profile Card
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 24),
-              padding: const EdgeInsets.symmetric(vertical: 24),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                children: [
-                  // ==== Profile Image with Edit Icon ====
-                  Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      const CircleAvatar(
-                        radius: 45,
-                        backgroundImage: AssetImage('assets/images/user.jpg'), // عدل هنا حسب صورتك
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.circle,
-                            border: Border.all(color: Colors.grey.shade300),
-                          ),
-                          padding: const EdgeInsets.all(4),
-                          child: const Icon(
-                            Icons.edit,
-                            size: 16,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 16),
-
-                  // ==== Name ====
-                  const Text(
-                    "Amir Yousry",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-
-                  const SizedBox(height: 4),
-
-                  // ==== Username ====
-                  const Text(
-                    "@amir.ayo1",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width - 50,
-              //height: widget.height,
-              decoration: BoxDecoration(
-                color: Color(0xFFFFFFFF),
-                borderRadius: BorderRadius.circular(24),
-
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
+              // Profile Card
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 24),
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
                 child: Column(
                   children: [
-                    Text('Personal Information'),
-                    Divider(height: 1, color: Color(0xFFF4F4F4),),
-                    UsernameTextField(
-                      title: 'Full Name',
-                      hintTitle: 'Amir Yousry',
+                    // ==== Profile Image with Edit Icon ====
+                    Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        const CircleAvatar(
+                          radius: 45,
+                          backgroundImage: AssetImage('assets/images/user.jpg'),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.grey.shade300),
+                            ),
+                            padding: const EdgeInsets.all(4),
+                            child: const Icon(
+                              Icons.edit,
+                              size: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    // ==== Name ====
+                    const Text(
+                      "Ahmed M. Aly",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+
+                    const SizedBox(height: 4),
+
+                    // ==== Username ====
+                    const Text(
+                      "ahmaly555@gmail.com",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
+              Container(
+                width: double.infinity,
+                //height: widget.height,
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                  borderRadius: BorderRadius.circular(24),
+
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text('Personal Information'),
+                      Divider(height: 1, color: Color(0xFFF4F4F4),),
+                      DefaultTextField(
+                        controller: _nameController,
+                        fieldType: DefaultTextFieldType.name,
+                        showEditIcon: true,
+                        isReadOnly: true,
+                        hintText: 'Ahmed M. Aly',
+                        onEditPressed: () {
+                          // حفظ التغييرات
+                        },
+                      ),
+                      SizedBox(height: 20,),
+                      UsernameTextField(
+                        title: 'E-Mail',
+
+                        hintTitle: 'ahmaly555@gmail.com',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
