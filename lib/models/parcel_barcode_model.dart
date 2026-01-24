d5d14c5c7e33216b072b541b40b736b238172de6
AhmedAly555 <ahmaly555@gmail.com>
2026-01-24 16:08:31 +0200

    feat: Enhance ParcelByBarcodeModel with additional fields and update parcel details screens for consistency

--- lib/models/parcel_barcode_model.dart
+++ lib/models/parcel_barcode_model.dart
@@ -1,5 +1,8 @@
 class ParcelByBarcodeModel {
   final int id;
+  final String clientName;
+  final String customerName;
+  final String parcelBarcode;
   final String pudoAddress;
   final String pudoName;
   final String responsibleName;
@@ -7,6 +10,9 @@ class ParcelByBarcodeModel {
 
   ParcelByBarcodeModel({
     required this.id,
+    required this.clientName,
+    required this.customerName,
+    required this.parcelBarcode,
     required this.pudoAddress,
     required this.pudoName,
     required this.responsibleName,
@@ -16,6 +22,9 @@ class ParcelByBarcodeModel {
   factory ParcelByBarcodeModel.fromJson(Map<String, dynamic> json) {
     return ParcelByBarcodeModel(
       id: json['id'] ?? 0,
+      clientName: json['client_name'] ?? '',
+      customerName: json['customer_name'] ?? '',
+      parcelBarcode: json['parcel_barcode'] ?? '',
       pudoAddress: json['pudo_address'] ?? '',
       pudoName: json['pudo_name'] ?? '',
       responsibleName: json['responsible_name'] ?? '',
